3532c873bb7201e092eb3dee9982f1a868f112a6
Ilya Maximov <xc.navi@gmail.com>
2022-12-15 10:38:12 +0200

    BinaryOperations

---
+++ src/BinaryOperations.java
@@ -0,0 +1,21 @@
+public class BinaryOperations {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 0;
+        System.out.println(a + b * 10); //260
+        System.out.println(255); //255
+        System.out.println(4 << 1); //8
+        System.out.println(14); //14
+        System.out.println(true && true);
+        System.out.println(0000);
+        System.out.println(0001);
+        System.out.println(0100);
+
+
+
+
+    }
+}
